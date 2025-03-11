809689d5b7642e9b81f22a6a2a2a3d34cdde0db5
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-03-11 22:11:06 +0200

    Implemented fetching events for group for cist API client

--- packages/cist_api/lib/src/cist_api.dart
+++ packages/cist_api/lib/src/cist_api.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:events_api/events_api.dart';
 import 'package:groups_api/groups_api.dart';
 import 'package:win1251/win1251.dart';
 
 /// API client class for `cist.nure.ua`.
-class CistApi implements GroupsApi {
+class CistApi implements GroupsApi, EventsApi {
   CistApi({required Dio dio}) : _dio = dio;
 
   final Dio _dio;
@@ -28,11 +29,58 @@ class CistApi implements GroupsApi {
     }
 
     final List<Faculty> result = [];
-    for (final faculty in json['university']['faculties']) {
-      result.add(Faculty.fromJson(faculty));
+    for (final facultyJson in json['university']['faculties']) {
+      result.add(Faculty.fromJson(facultyJson));
     }
     if (result.isEmpty) throw GroupsNotFoundFailure();
 
     return result;
+  }
+
+  @override
+  Future<({List<Event> events, List<Subject> subjects, List<Type> types})>
+  fetchEventsForGroup(int groupID, int fromTimestamp, int toTimestamp) async {
+    final response = await _dio.getUri(
+      Uri.https(_domain, '/ias/app/tt/P_API_EVEN_JSON', {
+        'type_id': ['1'],
+        'timetable_id': [groupID.toString()],
+        'time_from': [fromTimestamp.toString()],
+        'time_to': [toTimestamp.toString()],
+        'idClient': ['KNURESked'],
+      }),
+    );
+    if (response.statusCode != HttpStatus.ok) throw EventsRequestFailure();
+
+    final Map<String, dynamic> json;
+    try {
+      final jsonString = response.data.toString().from1251();
+      json = jsonDecode(jsonString);
+    } catch (_) {
+      throw EventsRequestFailure();
+    }
+
+    final List<Event> events = [];
+    for (final eventJson in json['events']) {
+      events.add(Event.fromJson(eventJson));
+    }
+
+    final List<Subject> subjects = [];
+    for (final subjectJson in json['subjects']) {
+      subjects.add(Subject.fromJson(subjectJson));
+    }
+
+    final List<Type> types = [];
+    for (final typeJson in json['types']) {
+      types.add(Type.fromJson(typeJson));
+    }
+
+    return (events: events, subjects: subjects, types: types);
+  }
+
+  @override
+  Future<({List<Event> events, List<Subject> subjects, List<Type> types})>
+  fetchEventsForTeacher(int teacherID, int fromTimestamp, int toTimestamp) {
+    // TODO: implement fetchEventsForTeacher
+    throw UnimplementedError();
   }
 }
