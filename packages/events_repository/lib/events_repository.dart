407ed05cbf39652d733ddcd2832964e2903997ba
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-03-22 02:50:22 +0200

    Renamed repository package

--- packages/events_repository/lib/events_repository.dart
+++
@@ -1,5 +0,0 @@
-/// Repository package for events.
-library;
-
-export 'src/events_repository.dart';
-export 'src/models/models.dart';
--- packages/university_repository/lib/src/university_repository.dart
+++ packages/university_repository/lib/src/university_repository.dart
@@ -1,12 +1,12 @@
 import 'package:events_api/events_api.dart' hide Event;
-import 'package:groups_api/groups_api.dart';
+import 'package:groups_api/groups_api.dart' hide Group;
 import 'package:rxdart/rxdart.dart';
-import 'package:teachers_api/teachers_api.dart';
+import 'package:teachers_api/teachers_api.dart' hide Teacher;
 
 import 'models/models.dart';
 
-class EventsRepository {
-  EventsRepository({
+class UniversityRepository {
+  UniversityRepository({
     required EventsApi eventsApi,
     required GroupsApi groupsApi,
     required TeachersApi teachersApi,
@@ -17,9 +17,19 @@ class EventsRepository {
   final EventsApi _eventsApi;
   final GroupsApi _groupsApi;
   final TeachersApi _teachersApi;
+
   late final _eventsStreamController = BehaviorSubject<List<Event>>.seeded(
+    const [],
+  );
+  late final _groupsStreamController = BehaviorSubject<List<Group>>.seeded(
+    const [],
+  );
+  late final _teachersStreamController = BehaviorSubject<List<Teacher>>.seeded(
     const [],
   );
 
   Stream<List<Event>> get events => _eventsStreamController.asBroadcastStream();
+  Stream<List<Group>> get groups => _groupsStreamController.asBroadcastStream();
+  Stream<List<Teacher>> get teachers =>
+      _teachersStreamController.asBroadcastStream();
 }
