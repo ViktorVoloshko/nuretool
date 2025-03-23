2f3f31f883003d467a1ffe407618ed3dc8911837
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-03-23 04:42:10 +0200

    Added save and load functionality to local_db_api

--- packages/local_db_api/lib/src/local_db_api.dart
+++ packages/local_db_api/lib/src/local_db_api.dart
@@ -1,1 +1,51 @@
 import 'database/database.dart';
+
+class LocalDBApi {
+  LocalDBApi({required AppDatabase database}) : _database = database;
+
+  final AppDatabase _database;
+
+  Stream<List<Event>> loadEvents() =>
+      _database.select(_database.events).watch();
+  Stream<List<Subject>> loadSubjects() =>
+      _database.select(_database.subjects).watch();
+  Stream<List<Group>> loadGroups() =>
+      _database.select(_database.groups).watch();
+  Stream<List<Teacher>> loadTeachers() =>
+      _database.select(_database.teachers).watch();
+  Stream<List<EventType>> loadEventTypes() =>
+      _database.select(_database.eventTypes).watch();
+
+  Future<int> saveEvent(EventsCompanion event) =>
+      _database.into(_database.events).insertOnConflictUpdate(event);
+
+  Future<void> saveEvents(List<EventsCompanion> events) => _database.batch(
+    (batch) => batch.insertAllOnConflictUpdate(_database.events, events),
+  );
+  Future<void> saveSubjects(List<SubjectsCompanion> subjects) =>
+      _database.batch(
+        (batch) =>
+            batch.insertAllOnConflictUpdate(_database.subjects, subjects),
+      );
+  Future<void> saveGroups(List<GroupsCompanion> groups) => _database.batch(
+    (batch) => batch.insertAllOnConflictUpdate(_database.groups, groups),
+  );
+  Future<void> saveTeachers(List<TeachersCompanion> teachers) =>
+      _database.batch(
+        (batch) =>
+            batch.insertAllOnConflictUpdate(_database.teachers, teachers),
+      );
+  Future<void> saveEventTypes(List<EventTypesCompanion> eventTypes) =>
+      _database.batch(
+        (batch) =>
+            batch.insertAllOnConflictUpdate(_database.eventTypes, eventTypes),
+      );
+
+  Stream<List<Task>> loadTasks() => _database.select(_database.tasks).watch();
+
+  Future<int> saveTask(TasksCompanion task) =>
+      _database.into(_database.tasks).insertOnConflictUpdate(task);
+  Future<void> saveTasks(List<TasksCompanion> tasks) => _database.batch(
+    (batch) => batch.insertAllOnConflictUpdate(_database.tasks, tasks),
+  );
+}
