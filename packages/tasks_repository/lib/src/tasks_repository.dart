f76bd45ea8de87158e76807d21a1875b66446d4e
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-04-04 04:21:28 +0300

    Fixed Stream type in repository

--- packages/tasks_repository/lib/src/tasks_repository.dart
+++ packages/tasks_repository/lib/src/tasks_repository.dart
@@ -1,14 +1,18 @@
-import 'package:local_db_api/local_db_api.dart';
+import 'package:local_db_api/local_db_api.dart' show LocalDBApi;
 import 'package:rxdart/rxdart.dart';
+
+import 'models/models.dart';
 
 class TasksRepository {
   TasksRepository({required LocalDBApi localDBApi}) : _localDBApi = localDBApi;
 
   final LocalDBApi _localDBApi;
 
-  late final _tasksStreamController = BehaviorSubject<List>.seeded(const []);
+  late final _tasksStreamController = BehaviorSubject<List<Task>>.seeded(
+    const [],
+  );
 
-  Stream<List> get tasks => _tasksStreamController.asBroadcastStream();
+  Stream<List<Task>> get tasks => _tasksStreamController.asBroadcastStream();
 
   Future<dynamic> dispose() => _tasksStreamController.close();
 }
