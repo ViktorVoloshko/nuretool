faeebed881b4e4dc8373ec60bf28f39a146101e4
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-04-05 04:29:37 +0300

    Added save methods on tasks repo

--- packages/tasks_repository/lib/src/tasks_repository.dart
+++ packages/tasks_repository/lib/src/tasks_repository.dart
@@ -1,4 +1,4 @@
-import 'package:local_db_api/local_db_api.dart' show LocalDBApi;
+import 'package:local_db_api/local_db_api.dart' show LocalDBApi, TasksCompanion;
 import 'package:rxdart/rxdart.dart';
 
 import 'models/models.dart';
@@ -32,6 +32,22 @@ class TasksRepository {
 
   Stream<List<SuperTask>> get tasks =>
       _tasksStreamController.asBroadcastStream();
+
+  Future<void> saveTask(Task task, int supertaskID) =>
+      _localDBApi.saveTask(task.toDBModel(supertaskID));
+
+  Future<void> saveSuperTask(SuperTask task) =>
+      _localDBApi.saveTasks(task.toDBModels());
+
+  Future<void> saveTasks(Iterable<SuperTask> tasks) {
+    final result = <TasksCompanion>[];
+
+    for (final supertask in tasks) {
+      result.addAll(supertask.toDBModels());
+    }
+
+    return _localDBApi.saveTasks(result);
+  }
 
   Future<dynamic> dispose() async {
     await _tasksSubscription.cancel();
