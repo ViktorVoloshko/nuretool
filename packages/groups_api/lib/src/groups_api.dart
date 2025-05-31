991e462fc5c8fcc478664fd4210773fe44e2afe9
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-05-31 03:43:53 +0300

    Fixed typos in doc comments

--- packages/groups_api/lib/src/groups_api.dart
+++ packages/groups_api/lib/src/groups_api.dart
@@ -13,7 +13,7 @@ class GroupsRequestFailure implements Exception {}
 /// Used to notify about API not returning any groups after response is
 /// processed.
 ///
-/// Since that is practivally impossible, it should be possible to assume that
-/// something went wrong but no problem was detected (no other exceprion was
+/// Since that is practically impossible, it is safe to assume that
+/// something went wrong but no problem was detected (no other exception was
 /// thrown) until after response was deserialized successfully.
 class GroupsNotFoundFailure implements Exception {}
