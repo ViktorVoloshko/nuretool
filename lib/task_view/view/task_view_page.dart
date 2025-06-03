82cb4d9b1bf1f7c29fb556e0b32159a096c1e9b3
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-06-04 01:09:24 +0300

    Made modal bottom sheet on TaskView as low as possible

--- lib/task_view/view/task_view_page.dart
+++ lib/task_view/view/task_view_page.dart
@@ -35,61 +35,59 @@ class TaskViewView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<TaskViewBloc, TaskViewState>(
       builder:
-          (context, state) => SizedBox(
-            height: 180.0,
-            child: switch (state) {
-              TaskViewInitial() => const Center(
-                child: CircularProgressIndicator(),
-              ),
-              TaskViewLoading() => const Center(
-                child: CircularProgressIndicator(),
-              ),
-              TaskViewFailure() => throw UnimplementedError(),
-              TaskViewSuccess() => Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TaskTitleField(
-                    initialValue: state.task.title,
-                    errorText: switch (state.titleError) {
-                      null => null,
-                      TitleError.emptyOrWhitespace =>
-                        AppLocalizations.of(context)!.titleEmptyOrWhitespace,
-                    },
-                    onChanged:
-                        (value) => context.read<TaskViewBloc>().add(
-                          TaskViewDataChanged(
-                            title: value!,
-                            type: state.task.type,
-                            deadline: state.task.deadline,
-                          ),
+          (context, state) => switch (state) {
+            TaskViewInitial() => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            TaskViewLoading() => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            TaskViewFailure() => throw UnimplementedError(),
+            TaskViewSuccess() => Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TaskTitleField(
+                  initialValue: state.task.title,
+                  errorText: switch (state.titleError) {
+                    null => null,
+                    TitleError.emptyOrWhitespace =>
+                      AppLocalizations.of(context)!.titleEmptyOrWhitespace,
+                  },
+                  onChanged:
+                      (value) => context.read<TaskViewBloc>().add(
+                        TaskViewDataChanged(
+                          title: value!,
+                          type: state.task.type,
+                          deadline: state.task.deadline,
                         ),
-                  ),
-                  TaskDeadlineField(
-                    deadline: state.task.deadline,
-                    onDeadlineChanged:
-                        (value) => context.read<TaskViewBloc>().add(
-                          TaskViewDataChanged(
-                            title: state.task.title,
-                            type: state.task.type,
-                            deadline: value,
-                          ),
+                      ),
+                ),
+                TaskDeadlineField(
+                  deadline: state.task.deadline,
+                  onDeadlineChanged:
+                      (value) => context.read<TaskViewBloc>().add(
+                        TaskViewDataChanged(
+                          title: state.task.title,
+                          type: state.task.type,
+                          deadline: value,
                         ),
-                  ),
-                  TaskTypeSelectionWidget(
-                    selected: state.task.type,
-                    onSelectionChanged:
-                        (value) => context.read<TaskViewBloc>().add(
-                          TaskViewDataChanged(
-                            title: state.task.title,
-                            type: value.first,
-                            deadline: state.task.deadline,
-                          ),
+                      ),
+                ),
+                TaskTypeSelectionWidget(
+                  selected: state.task.type,
+                  onSelectionChanged:
+                      (value) => context.read<TaskViewBloc>().add(
+                        TaskViewDataChanged(
+                          title: state.task.title,
+                          type: value.first,
+                          deadline: state.task.deadline,
                         ),
-                  ),
-                ],
-              ),
-            },
-          ),
+                      ),
+                ),
+              ],
+            ),
+          },
     );
   }
 }
