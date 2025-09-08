cbbbfadde2fe04ca375068f613dd093ac872ded0
ViktorVoloshko <viktor.voloshko@outlook.com>
2025-09-08 13:50:33 +0300

    Fix semester boundaries

--- packages/university_repository/lib/src/utils/semester_date_time.dart
+++ packages/university_repository/lib/src/utils/semester_date_time.dart
@@ -1,14 +1,14 @@
 extension SemesterDateTime on DateTime {
   DateTime get startOfSemester => switch (month) {
     DateTime.january => DateTime(year - 1, DateTime.august, 1),
-    >= DateTime.september => DateTime(year, DateTime.august, 1),
+    >= DateTime.august => DateTime(year, DateTime.august, 1),
     > DateTime.january => DateTime(year, DateTime.february, 1),
     _ => throw StateError('not sure how you can even get here'),
   };
 
   DateTime get endOfSemester => switch (month) {
     DateTime.january => DateTime(year, DateTime.january, 31),
-    >= DateTime.september => DateTime(year + 1, DateTime.january, 31),
+    >= DateTime.august => DateTime(year + 1, DateTime.january, 31),
     > DateTime.january => DateTime(year, DateTime.july, 31),
     _ => throw StateError('not sure how you can even get here'),
   };
